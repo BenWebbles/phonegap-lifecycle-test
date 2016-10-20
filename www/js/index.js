81e49d4a060f2248ce87808a0d4a0f90850792c7
BenWebbles <WEBB1_15@uni.worc.ac.uk>
2016-10-20 10:40:38 +0100

    plsplsplsplsplsplsplspsl

--- www/js/index.js
+++ www/js/index.js
@@ -1,10 +1,20 @@
+var key = "pen";
+var value = window.localStorage.getItem(key);
 
+    function onLoad() {
+        window.localStorage.setItem(key, value);
+		console.log("on load");
+    }
 	
 
 
     // device APIs are available
     //
+    function onDeviceReady() {
+		window.alert(value);
+		console.log("device ready");
 
+    }
 
 
 
