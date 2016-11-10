1998835c080498b89404ebe0ae543f039261cb2e
BenWebbles <WEBB1_15@uni.worc.ac.uk>
2016-11-10 10:22:18 +0000

    yes

--- www/js/index.js
+++ www/js/index.js
@@ -1,42 +1,15 @@
-/*var key = "pen";
-var value = "black";
-var get = window.localStorage.getItem(key);
-
-    function onLoad() {
-        window.localStorage.setItem(key, value);
-		console.log("on load");
-		window.alert(get);
-	}
-	
-	
+$(document).on("pagecreate","#pageone",function(){
+  $('#submitButton').on("click", function(){
+    submitText();
+  });            
+});            
 
 
-    // device APIs are available
-    //
-    function onDeviceReady() {
-
-		console.log("device ready");
-
-    }
-*/
-var chocolate = {
-	"chocName": "Fudge",
-	"overview": {
-		"chocCal": "114kcal"
-		"chocFat": "3.4g"
-		"chocSalt": "0.10g"
-	}
-	
+function submitText() {
+	var text = $('#textinput').val();
+	alert(text);
 }
 
-var chocString = JSON.stringify(chocolate);
-window.localStorage.setItem("address", chocString)
-
-function onLoad() {
-	console.log(
-		chocolate.chocName +
-		"has "+
-		chocCal +
-		"amount of calories per bar!");
+function storeValue(key, value) {
+	//add some code to store the key-value pair in persistant storage 
 }
-
